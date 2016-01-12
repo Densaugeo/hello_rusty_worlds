6bbbf16a64b21bdfe6babaf716a2b94b066851ca
Your Name <you@example.com>
2016-01-11 22:58:20 -0500

    Sync docs with new description

--- search-index.js
+++ search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = {};
 searchIndex['hello_world'] = {"items":[],"paths":[]};
-searchIndex['hello_rusty_worlds'] = {"items":[[0,"","hello_rusty_worlds","The 'hello_rusty_worlds' crate is a hello world crate with automated testing, documentation,\ncontinuous integration, a published crate, tested example code, implemented as a library\nwith a command line tool. Includes examples of everything needed to make a new rust project\nthat follows best practices.",null,null],[5,"hello_world","","This function says hello to one of Sol's worlds, specified by a numeric argument",null,{"inputs":[{"name":"u8"}],"output":{"name":"option"}}]],"paths":[]};
+searchIndex['hello_rusty_worlds'] = {"items":[[0,"","hello_rusty_worlds","A hello world crate with automated testing, documentation,\ncontinuous integration, tested example code, implemented as a library\nwith a command line tool. Supports Sol and all its planets, not just 'Hello (unspecified) world!'",null,null],[5,"hello_world","","This function says hello to one of Sol's worlds, specified by a numeric argument",null,{"inputs":[{"name":"u8"}],"output":{"name":"option"}}]],"paths":[]};
 initSearch(searchIndex);
